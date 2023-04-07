8af9bdef4aa8933f8a107599aa3dbe008612555c
Diego Gutierrez <diego_ggv@pm.me>
2023-04-06 21:13:09 -0400

    add content, functionality and basic styling to hostVans and hostVanDetails pages

--- src/pages/Host/HostVanDetails.jsx
+++ src/pages/Host/HostVanDetails.jsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useParams } from "react-router-dom"
 
 function HostVanDetails() {
+  const params = useParams()
+  const [van, setVan] = useState(null)
+
+  useEffect(() => {
+    fetch(`/api/host/vans/${params.id}`)
+      .then(res => res.json())
+      .then(data => setVan(data.vans))
+      .catch(err => console.log(err))
+  }, [params.id])
+
   return (
-    <h1>Host Van Details</h1>
+    <div className="host_van_detail">
+      <img src={ van.imageUrl } alt={ van.name }/>
+      
+    </div>
   )
 }
 
--- src/pages/Host/HostVans.jsx
+++ src/pages/Host/HostVans.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+import { Link } from 'react-router-dom'
 
 function HostVans() {
-  return (
-    <h1>Host Vans</h1>
-  )
+	// REACT HOOKS
+	// ==============================
+	const [vans, setVans] = useState([])
+
+	useEffect(() => {
+		fetch("/api/host/vans")
+			.then(res => res.json())
+			.then(data => setVans(data.vans))
+			.catch(err => console.log(err))
+	}, [])
+
+
+	//	RENDER
+	// ==============================
+	const hostVans = vans.map(van => {
+		return (
+			<div key={van.id} className="host_van">
+				<Link to={`/host/vans/:id`} >
+					<img src={van.imageUrl} alt={van.name} />
+					<div>
+						<h2>{van.name}</h2>
+						<p>${van.price}/day</p>
+					</div>
+				</Link>
+			</div>
+		)
+	})
+
+	return (
+		<div className="host_vans_container">
+			<h2>Your listed vans</h2>
+			{hostVans}
+		</div>
+	)
 }
 
 export default HostVans
